4ffa639a6fd3d26098f2faf9053b3f32656784a0
Kisanth-21 <Kisanth.R@ust.trumio.ai>
2025-12-27 18:45:02 +0530

    Toll Controller

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
-using AIToolFinderApp.Services;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-
-
 builder.Services.AddControllers();
 
 var app = builder.Build();
