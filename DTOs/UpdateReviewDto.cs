e15b6f9e4dd9de2fc0affba2ddb3b19d1c179444
Anna Dejo(UST <304487@ust.com>
2025-12-27 16:41:05 +0530

    Updated AdminController

--- DTOs/UpdateReviewDto.cs
+++
@@ -1,7 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public class UpdateReviewDto
-{
-    [Required(ErrorMessage = "Status is required")]
-    public ReviewStatus? Status { get; set; }
-}
