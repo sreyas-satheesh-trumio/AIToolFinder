c745156e8896fbfa8c146362b4a52e78ce38f34e
Pasupathi <Pasupathi.U@ust.trumio.ai>
2025-12-27 18:49:21 +0530

    Controller

--- Controllers/ReviewsController.cs
+++ Controllers/ReviewsController.cs
@@ -1,22 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
- 
+
 [ApiController]
 public class ReviewsController : ControllerBase
 {
     private readonly JsonFileService<Review> _reviewRepo = new("Data/reviews.json");
     private readonly ToolService _toolService = new();
- 
+
     [HttpPost("review")]
     public IActionResult SubmitReview(Review review)
     {
         review.Id = Guid.NewGuid().GetHashCode();
         review.Status = "Pending";
- 
+
         var reviews = _reviewRepo.Read();
         reviews.Add(review);
         _reviewRepo.Write(reviews);
- 
+
         return Ok("Review submitted for approval");
-    }
+          }
 }
- 
---
+++ Controllers/ToolsController.cs
@@ -0,0 +1,17 @@
+    using Microsoft.AspNetCore.Mvc;
+    [ApiController]
+    [Route("tools")]
+    public class ToolsController : ControllerBase
+    {
+        private readonly ToolService _service = new();
+    
+        [HttpGet]
+        public IActionResult GetTools(
+            [FromQuery] string? category,
+            [FromQuery] string? price,
+            [FromQuery] double? rating)
+        {
+            var result = _service.GetTools(category, price, rating);
+            return Ok(result);
+        }
+    }
