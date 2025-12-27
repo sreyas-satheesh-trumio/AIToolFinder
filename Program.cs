3272eed15c65cd6991e48620ec4b523554913404
sreyas-satheesh-trumio <sreyas.satheesh@ust.trumio.ai>
2025-12-27 14:56:04 +0530

    feat: add di of toolservice and reviewservice

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using AIToolFinderApp.Services;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<IToolService, ToolService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+
 builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
 app.UseRouting();
 app.MapControllers();
 app.UseStaticFiles();
+
 app.Run();
