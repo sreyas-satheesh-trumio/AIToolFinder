f212b1a9d21e782aed05ab76f7b8c1ca6e593abd
sreyas-satheesh-trumio <sreyas.satheesh@ust.trumio.ai>
2025-12-29 13:59:17 +0530

    feat: add json serializer to the enum for string serialization

--- Enums/ApprovalStatus.cs
+++ Enums/ApprovalStatus.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace AIToolFinder.Services
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ApprovalStatus
     {
         Pending,
